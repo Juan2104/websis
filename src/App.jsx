26f9c4246f9a9b8c9212e8c92e7ac06cd2472688
JuanJRuiz <juaruiz@andreani.com>
2025-01-29 16:30:43 -0300

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import Navbar from './components/Navbar';
 import HomeSection from './sections/HomeSection';
 import PortfolioSection from './sections/PortfolioSection';
 import ContactSection from './sections/ContactSection';
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-MTBE6JJ3H0"; // Reemplaza con tu ID de Google Analytics
+
+ReactGA.initialize(TRACKING_ID);
+
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.send("pageview"); // Envía un evento cada vez que se carga la página
+  }, []);
   return (
     <div>
       <Navbar />
